92b4157ba796e3b9f9c329592866597558774f3e
Sigrid Stensvold <sigridstens@gmail.com>
2019-02-24 11:22:38 -0600

    Created window prompt to ask for user's name with console.log responses using if else statement

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-//For readability and clarity it is good practice to declare variables at the beginning of the JS document if possible
 var churchillSpeech = {
-      'author': 'Churchill',
-      'year': 1940,
-      'yearIsBCE': false,
-      'authorAge': '66'
+        'author': 'Churchill',
+        'year': 1940,
+        'yearIsBCE': false,
+        'authorAge': '66'
     },
     ghandiSpeech = {
-      'author': 'Ghandi',
-      'year': 1942,
-      'yearIsBCE': false,
-      'authorAge': '73'
+        'author': 'Ghandi',
+        'year': 1942,
+        'yearIsBCE': false,
+        'authorAge': '73'
     },
     demosthenesSpeech = {
-      'author': 'Demosthenes',
-      'year': 342,
-      'yearIsBCE': true,
-      'authorAge': '42'
+        'author': 'Demosthenes',
+        'year': 342,
+        'yearIsBCE': true,
+        'authorAge': '42'
     },
-    speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech];
+    speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech],
 
-document.getElementById('BtnDonate').addEventListener('click', function(){
+    userName = window.prompt("Why hello! What is your name?");
+
+if (typeof userName !== "string") {
+    userName = window.prompt("Quit playing games! We just want to say hello. What is your name?");
+} else if (userName === "") {
+    console.log("Okay, I'll just call you User");
+    userName = "User";
+} else {
+    console.log("Hi " + userName + "!");
+}
+
+    document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
 });
 
